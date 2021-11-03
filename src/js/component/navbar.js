3b6985940a2812ce72437454be68aba7175b3874
MagcDEV <manuel117g@gmail.com>
2021-11-03 09:49:12 -0400

    la mierda del dropdown no agarra

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starWarsLogo from "../../img/star-wars-logo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				<img src={starWarsLogo} width="140" height="60" alt="" />
+			</a>
+			<div className="dropdown">
+				<button
+					className="btn btn-primary dropdown-toggle"
+					type="button"
+					id="dropdownMenuButton"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					Dropdown button
+				</button>
+				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<a className="dropdown-item" href="#">
+						Action
+					</a>
+					<a className="dropdown-item" href="#">
+						Another action
+					</a>
+					<a className="dropdown-item" href="#">
+						Something else here
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
